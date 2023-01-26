16ab914bf535811c86448890fe5788041eae05f2
Jose Lara <jose.lara@gmail.com>
2023-01-26 04:52:26 -0800

    Update favicon to emoji

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ useHead({
     {
       rel: 'icon',
       type: 'image/svg+xml',
-      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+      href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🎯</text></svg>',
     },
   ],
 })
