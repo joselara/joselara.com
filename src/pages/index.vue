4e2132de2215f09e395add7b21b85734e1bfa9b4
Jose Lara <jose@joselara.com>
2024-01-18 12:16:11 -0800

    Switch up wording

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,7 +9,7 @@ defineOptions({
     <div class="mt-10">
       <Container>
         <p class="text-zinc-600">
-          Highly skilled software architect with over 15 years of experience in software design, network architecture, and coding, passionate about solving complex challenges and creating innovative solutions. I am actively seeking opportunities to collaborate with exceptional teams in the pursuit of cutting-edge product development. Beyond my professional pursuits, I thrive in the world of containerized environments and cherish moments with my two little pups, Lola and Fluffy. My mission is to continue pushing the boundaries of technology while fostering a collaborative and innovative work culture.
+          Highly skilled software architect with over 15 years of experience in software design, network architecture, and coding, passionate about solving complex challenges and creating innovative solutions. I am actively seeking opportunities to collaborate with exceptional teams in the pursuit of cutting-edge product development. My mission is to continue pushing the boundaries of technology while fostering a collaborative and innovative work culture. Beyond my professional pursuits, I thrive in the world of containerized environments and cherish moments with my two little pups, Lola and Fluffy.
         </p>
       </Container>
     </div>
