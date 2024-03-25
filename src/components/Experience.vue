9e040e08eb407d80660f1f77e6a8021dd4c448d3
Jose Lara <jose@joselara.com>
2024-03-24 23:19:33 -0700

    Update Experience.vue styles for bullets list spacing

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -13,7 +13,7 @@ defineProps<{
 
 <template>
   <div class="sm:flex gap-10 w-full">
-    <div class="text-sm text-zinc-500 mt-1">
+    <div class="text-sm text-zinc-500 mt-1 tracking-tight">
         {{ start }} &mdash; {{ end }}
     </div>
     <div class="flex-1">
@@ -24,8 +24,8 @@ defineProps<{
         <p v-if="description">
           {{ description }}
         </p>
-        <ul v-if="bullets" role="list" class="list-disc pl-4 -ml-4">
-          <li v-for="(bullet, index) in bullets" :key="index" class="mb-2 tracking-tight leading-relaxed">
+        <ul v-if="bullets" role="list" class="list-disc pl-4 -ml-4 list-none">
+          <li v-for="(bullet, index) in bullets" :key="index" class="mb-2.5 tracking-tight leading-relaxed">
             {{ bullet }}
           </li>
         </ul>
